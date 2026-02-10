56e1406f307039c326170be37545f188b9ecb154
CAFE2l <pciskolargx@gmail.com>
2026-02-10 15:44:43 -0300

    variaveis adicionadas

---
+++ php/Aulas/Aula04/variaveis.php
@@ -0,0 +1,23 @@
+<?php
+  $email = "gutiajs@gmail.com";
+echo $email;
+
+
+
+$email = "pciskolargx@gmail.com";
+
+echo"<br>";
+
+echo $email;
+
+echo "<br>";
+
+$email = 100;
+echo $email;
+
+
+$nome = "Gabriel Felipe";
+echo "<br>";
+echo $nome;
+
+
