3e6a11e09227238f04f68af841ad57e23b0d591c
CAFE2l <pciskolargx@gmail.com>
2026-02-10 18:35:48 -0300

    aprendendo estruturas condicionais com php

---
+++ php/Aulas/Aula10/condicionais/if.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+/**/
+/* if ($a > $b){ */
+/*   echo "BRUTAL"; */
+/*   echo "<br>"; */
+/*   echo "Acabou pro beta"; */
+/* } else{ */
+/*   echo "Não sobrou nada :O"; */
+/* } */
+/**/
+if ($a > $b)
+ echo "<br>";
+echo "BRUTAL<br>";
+echo "Acabou pro beta";
+
