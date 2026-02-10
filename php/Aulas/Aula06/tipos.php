bad9b88c2efe7e46dc75c5d8cfa71fae27249b11
CAFE2l <pciskolargx@gmail.com>
2026-02-10 16:42:55 -0300

    mexendo com tipos

---
+++ php/Aulas/Aula06/tipos.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Elton afonso";
+$idade = 20;
+$preco = 39.90;
+$casada = true;
+
+echo "a variavel nome é do tipo: ";
+echo gettype($nome);
+
+$nome = false;
+echo "<br>"; 
+
+echo "a variavel nome é do tipo: ";
+echo gettype($nome);
+
+
