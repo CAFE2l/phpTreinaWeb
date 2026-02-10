d33049f11a8e27e7758fc54c2bbfe86759646a3b
CAFE2l <pciskolargx@gmail.com>
2026-02-10 14:59:55 -0300

    primeiro commit

---
+++ php/Aulas/Aula02/olamundo.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "Ola mundo PHP!";
+echo "<br>";// nao sabia disso
+echo "Nova Linha";
---
+++ php/Aulas/Aula03/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="" rel="stylesheet">
+  </head>
+  <body>
+  <h1>Minha primeira página com php</h1>
+  <?php echo "tinha que ser beta mesmo";?>
+  <p>estou no curso de <?php echo "PHP";?></p>
+
+  <p>estou no curso de <?="JAVA ((0_0))"?></p>
+
+  <p>10 mais 10 é igual a <?php echo 10 + 10;?></p>
+  <p>10 mais 10 é igual a <?php echo 10 + 130;?></p>
+
+  
+
+  <?php
+  echo "linha 1<br>";
+  echo "linha 2<br>";
+  echo "linha 3<br>";
+    ?>
+  </body>
+</html>
