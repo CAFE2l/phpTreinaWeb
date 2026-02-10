e4fb37451083da861b931a2a935c8245824bfdc6
CAFE2l <pciskolargx@gmail.com>
2026-02-10 18:07:30 -0300

    operação logica com a tabela verdade

---
+++ php/Aulas/Aula09/Operadores/logicos.php
@@ -0,0 +1,24 @@
+<?php
+$a = false;
+$b = true;
+
+// && and
+/**/
+/* var_dump($a && $b); */
+/* echo "<br>"; */
+/**/
+/* var_dump($a and $b); */
+/* echo "<br>"; */
+
+// or 2 pipes (nao existe no meu teclado '-')
+/**/
+/* var_dump($a or $b); */
+/* echo "<br>"; */
+
+/* var_dump($a xor $b); */
+/* echo "<br>"; */
+
+// not !)
+
+var_dump(!$a);
+echo "<br>";
