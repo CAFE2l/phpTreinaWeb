0a49ec941e382bf376d2b483df636428e64e76fa
CAFE2l <pciskolargx@gmail.com>
2026-02-11 16:01:34 -0300

    learning about errors

---
+++ php/Aulas/Aula13/erros/tipos.php
@@ -0,0 +1,22 @@
+<?php
+
+error_reporting(E_ALL);
+//Notice
+/* $valor1 = 10; */
+/* echo $valor; */
+//Warning
+//$numero = NAO_EXISTE;
+
+//Fatal Error
+/* function somar($valor1, $valor2){ */
+/*   return $valor2 + $valor1; */
+/* } */
+/**/
+/* function somar($valor1, $valor2){ */
+/*   return $valor2 + $valor1; */
+/* } */
+//Parser Error
+//echo "asdsadasd"
+
+
+echo "chegou ao fim do programa";
