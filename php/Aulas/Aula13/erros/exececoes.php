b75d62ee4663c073b54d2b30549006a8ce355f21
CAFE2l <pciskolargx@gmail.com>
2026-02-11 16:10:17 -0300

    learning how to display erros in the screen

---
+++ php/Aulas/Aula13/erros/exececoes.php
@@ -0,0 +1,19 @@
+<?php
+
+error_reporting(E_ALL);
+
+$divisor = 2;
+
+
+try{
+  if($divisor === 0){
+    throw new Exception("Não é possível dividir por 0", 1);
+  }
+
+  $valor = 100 / $divisor;
+  echo $valor;
+} catch (Throwable $th){
+  echo $th->getMessage();
+}
+
+
