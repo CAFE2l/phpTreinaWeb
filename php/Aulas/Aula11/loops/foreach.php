5015fc0eaeb4ef584736c094e1cd844aa6a299c2
CAFE2l <pciskolargx@gmail.com>
2026-02-11 14:55:20 -0300

    learning the foreach loop structure

---
+++ php/Aulas/Aula11/loops/foreach.php
@@ -0,0 +1,12 @@
+<?php
+
+$numeros = [2, 3, 5, 2, 7, 9, 10];
+
+/* foreach ($numeros as $key => $value){ */
+/*   echo "key: $key - valor: {$numeros[$key]}<br>"; */
+/**/
+/* } */
+
+foreach($numeros as $valor){
+  echo "valor: {$valor}<br>";
+}
