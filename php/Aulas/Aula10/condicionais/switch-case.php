1d40a6a8e6bd47299a5b9ecf818d5112c6b39d8f
CAFE2l <pciskolargx@gmail.com>
2026-02-11 14:32:24 -0300

    conditions with swtich case

---
+++ php/Aulas/Aula10/condicionais/switch-case.php
@@ -0,0 +1,43 @@
+<?php
+$a = 6;
+
+
+switch ($a){
+  case 0:
+   echo "é beta";
+   break;
+  case 1: 
+   echo "é betinha";
+   break;
+  case 2: 
+   echo "Tinha que ser o chaves mesmo";
+   break;
+  case 3: 
+   echo "chad";
+   break;
+  case 4:
+  case 5:
+  case 6:
+    echo "Tenho um valor de 4 a 6";
+    break;
+  default: 
+   echo "O MAIS BETA POSSIVEL";
+}
+echo "<br>";
+$curso = "PHP";
+
+switch ($curso){
+case 'PHP':
+  echo "curso de php, é back-end";
+  break;
+case 'JavaScript':
+  echo "curso de javascript";
+  break;
+default: 
+  echo "outro curso";
+  break;
+
+}
+
+
+
